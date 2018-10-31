797b05c60e593f7f89296ead257f63d09f867266
Jesse Evans <jesseelizabeth1@gmail.com>
2018-10-31 01:02:30 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let tracker = 0
 
 function onKeyDown(event) {
-  const keyCode = event.which
+  const keyCode = event.which || event.location
   
 // 1. figure out where in the sequence they are 
   if (keyCode === code[tracker]) {
